0cd95c30d3da8f5b38c207c2ad5f704a4e688146
John Forster <johnforsterbusiness@gmail.com>
2023-11-11 16:50:16 -0500

    set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './pages/login/login.jsx';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import GenerateResume from './pages/generateResume/generateResume.jsx';
+import Cookies from 'universal-cookie';
+
+
 
 function App() {
+
+  const cookieHandler = new Cookies();
+
+  const uid = cookieHandler.get('uid')
+
   return (
     <div className="App">
-      <Login></Login>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/generateResume" element={<GenerateResume />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/generateResume/generateResume.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function generateResume() {
+    return (
+        <div>
+            <button>Generate Resume</button>
+            <input type="text" placeholder="Enter job description" />
+        </div>
+    );
+}
+
+export default generateResume;

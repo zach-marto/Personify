cb6b803ba1deda546b3be240b18b9fc28d9f2d05
John Forster <johnforsterbusiness@gmail.com>
2023-11-11 17:32:07 -0500

    Set up GenerateResume page logic

--- client/src/pages/generateResume/generateResume.jsx
+++ client/src/pages/generateResume/generateResume.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
 
-function generateResume() {
+function GenerateResume() {
+
+    const cookieHandler = new Cookies();
+    const uid = cookieHandler.get('uid');
+    const [jobDescription, setJobDescription] = useState("");
+
+    const generateNewResume = () => {
+      console.log("Generating new resume");
+      //Send job description to backend for processing
+      axios.post('/generate', {
+        jobDescription: jobDescription,
+        uid: uid
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error generating resume', error);
+      });
+    }
+     
     return (
         <div>
-            <button>Generate Resume</button>
-            <input type="text" placeholder="Enter job description" />
+            <button onClick={generateNewResume}>Generate Resume</button>
+            <textarea type="text" placeholder="Enter job description" onChange={(e) => setJobDescription(e.target.value)} />
         </div>
     );
 }
 
-export default generateResume;
+export default GenerateResume;

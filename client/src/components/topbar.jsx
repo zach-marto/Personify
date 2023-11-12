00f4c1872e9945e1c14da80092d0bb1ff18bdb23
John Forster <johnforsterbusiness@gmail.com>
2023-11-12 09:15:36 -0500

    add logo

--- client/src/components/topbar.jsx
+++ client/src/components/topbar.jsx
@@ -17,12 +17,13 @@ function Topbar() {
 
   return (
     <div className="topbar" style={{display:"flex",  justifyContent:"stretch"}}>
-     <div className='topbar-left'>
-      <h1 onClick={navigateToGenerateResume}>Personify</h1>
-     </div>
-     <div className='topbar-right'>
-      <Button variant="contained" onClick={navigateToGenerateResume}>Generate Resume</Button>
-      <Button variant="contained" onClick={navigateToProfile}>My Profile</Button>
+      <div className='topbar-left'>
+        <h1 onClick={navigateToGenerateResume}>Personify</h1>
+        <img className='logo' src="https://lh3.googleusercontent.com/pw/ADCreHcN_0M67MKEj2Iot9acHVVLIMgvYtW4scJvDxFe81R6hJCmUctgHC8aiGBjQCUZ5FTr8nAHcyTvghcnOIIk7WOdd6_QFpV7z3LizJMkfhH5qeUFYykD6p8oFLy6EnnJRQM5LiSSmAP0IdvtZvgAuVyg=w345-h372-s-no"></img>
+      </div>
+      <div className='topbar-right'>
+        <Button variant="contained" onClick={navigateToGenerateResume}>Generate Resume</Button>
+        <Button variant="contained" onClick={navigateToProfile}>My Profile</Button>
       </div>
     </div>
   )

9adc1247f7f4bd6280658b099ddccbfbb7b0c9d9
Jeremy Halzel <jhalzel@umass.edu>
2023-11-11 19:08:18 -0500

    update app.js
    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import GenerateResume from './pages/generateResume/generateResume.jsx';
 import Cookies from 'universal-cookie';
 import EditProfile from './pages/profile/EditProfile.jsx';
-import uid from './cookieHandler';
+import uid from './cookieHandler.js';
 
 function App() {
 
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={uid ? <Navigate to="/generateResume" /> : <Login />} />
-          <Route path="/generateResume" element={uid ? <GenerateResume /> : <Navigate to="/" />} />
+          <Route path="/" element={uid ? <Navigate to="/generate" /> : <Login />} />
+          <Route path="/generate" element={uid ? <GenerateResume /> : <Navigate to="/" />} />
           <Route path="/editProfile" element={uid ? <EditProfile /> : <Navigate to="/" />} />
         </Routes>
       </Router>
